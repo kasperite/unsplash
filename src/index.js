22443078c04b726cdb6ef2afc3ff25b6297b8b14
Lukas Nguyen <lukas.nguyen9288@gmail.com>
2017-02-17 12:41:56 +1300

    Added webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+let main = document.getElementById("content");
+ReactDOM.render(<App />, main);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 var path = require("path");
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "js")
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
   }
 };
